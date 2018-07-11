84c6d398f3d90745663be25917a22801e366840f
Sanju Thomas <thomas.sanju@gmail.com>
2018-07-11 14:08:28 -0400

    Update TestEmployeeController.java

--- src/test/java/com/ourownjava/spring/web/TestEmployeeController.java
+++ src/test/java/com/ourownjava/spring/web/TestEmployeeController.java
@@ -19,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
- * @author ourownjava.com
+ * 
  * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
